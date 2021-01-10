f2950b8bb62d51afc3a8914732221bbd9b2c7301
kosalanuwan <kosalanuwan@users.noreply.github.com>
2021-01-10 19:20:24 +0000

    deploy: 90787077ea5534bd0e344cecf86cc8bb18a89081

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/jekyll-otto-theme/assets/css/main.css?202101091608",
+        "/jekyll-otto-theme/assets/css/main.css?202101101920",
         "/jekyll-otto-theme/"
       ]);
     })
